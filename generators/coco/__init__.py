88c707ef9d9f93790330754f50c2bd5f8c2ea058
vcarpani <v.carpani@fizyr.com>
2019-07-02 17:40:21 +0200

    Return multiple generators.

--- generators/coco/__init__.py
+++ generators/coco/__init__.py
@@ -1,4 +1,10 @@
 from .generator import CocoGenerator
 
 def from_config(config, **kwargs):
-	return CocoGenerator(config['data_dir'], config['set_name'], **kwargs)
+	generators = {}
+	if config['train_set_name'] is not None:
+		generators['train'] = CocoGenerator(config['data_dir'], config['train_set_name'], **kwargs)
+
+	if config['validation_set_name'] is not None:
+		generators['validation'] = CocoGenerator(config['data_dir'], config['validation_set_name'], **kwargs)
+	return generators
