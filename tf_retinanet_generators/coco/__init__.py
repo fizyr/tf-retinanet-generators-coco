9635ce966b72beefb2d73bf968642f5ae7a81e65
vcarpani <v.carpani@fizyr.com>
2019-08-23 10:06:29 +0200

    Pass evaluation function.

--- tf_retinanet_generators/coco/__init__.py
+++ tf_retinanet_generators/coco/__init__.py
@@ -1,14 +1,19 @@
 from .generator import CocoGenerator
+from .eval      import evaluate_coco
 from .eval      import CocoEval
 
 
 def from_config(config, **kwargs):
 	generators = {}
-	if config['train_set_name'] is not None:
+	if 'train_set_name' in config and config['train_set_name'] is not None:
 		generators['train'] = CocoGenerator(config['data_dir'], config['train_set_name'], **kwargs)
 
-	if config['validation_set_name'] is not None:
+	if 'validation_set_name' in config and config['validation_set_name'] is not None:
 		generators['validation'] = CocoGenerator(config['data_dir'], config['validation_set_name'], **kwargs)
 
-	generators['custom_evaluation'] = CocoEval
+	if 'test_set_name' in config and config['test_set_name'] is not None:
+		generators['test'] = CocoGenerator(config['data_dir'], config['test_set_name'], **kwargs)
+
+	generators['custom_evaluation']          = evaluate_coco
+	generators['custom_evaluation_callback'] = CocoEval
 	return generators
